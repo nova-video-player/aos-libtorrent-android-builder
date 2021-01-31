b3dc7564278dbf3554f9c11196f429a6aa9b807e
Courville Software <software@courville.org>
2021-01-31 18:36:11 +0100

    WIP boost 1.75.0 migration

--- build.sh
+++ build.sh
@@ -52,14 +52,15 @@ fi
 
 cd libtorrent
 
-export BOOST_VERSION=1_72_0
+export BOOST_VERSION=1_75_0
 
 export BOOST_ROOT=${REPO_TOP_DIR}/native/boost/boost_${BOOST_VERSION}
 BOOST=boost_${BOOST_VERSION}
 
 export BOOST_BUILD_PATH=$REPO_TOP_DIR/native/boost/boost_${BOOST_VERSION}-${ABI}
 
-$BOOST_ROOT/b2 \
+cd $BOOST_ROOT
+./b2 \
     --build-dir=../../boost/${BOOST}-${ABI} \
     --stagedir=../../boost/${BOOST}-${ABI}/stage \
     --user-config=../../boost/${BOOST}-${ABI}/user-config.jam \
